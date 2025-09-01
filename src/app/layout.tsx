11df297b284f82ca86d6d5de4054184df2c86321
Afra Anjum <lilithkard77@gmail.com>
2025-09-01 12:49:35 +0600

    fixed font issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}inter antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} inter antialiased`}
       >
         <ThemeProvider
           attribute="class"
