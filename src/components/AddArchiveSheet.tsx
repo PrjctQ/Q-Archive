bf419b473978d30fbbf5959107d7fef9e7814b3e
Tahsin Alahi <alahitahsin@gmail.com>
2025-09-17 02:36:04 +0600

    refactor: reduced the states in add archive

--- src/components/AddArchiveSheet.tsx
+++ src/components/AddArchiveSheet.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import { useReducer } from "react";
 import dynamic from "next/dynamic";
 import "react-markdown-editor-lite/lib/index.css";
 import { useState } from "react";
@@ -32,16 +32,64 @@ const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
   ssr: false,
 });
 
+interface InitState {
+  title: string;
+  slug: string;
+  content: string;
+  date: Date | undefined;
+}
+
+const initState: InitState = {
+  title: "",
+  slug: "",
+  content: "",
+  date: undefined,
+};
+
+type Action =
+  | { type: "TITLE_UPDATE"; payload: string }
+  | { type: "SLUG_UPDATE"; payload: string }
+  | { type: "CONTENT_UPDATE"; payload: string }
+  | { type: "DATE_UPDATE"; payload: Date | undefined }
+  | { type: "RESET" };
+
+function reducer(state: InitState, action: Action) {
+  switch (action.type) {
+    case "TITLE_UPDATE":
+      return {
+        ...state,
+        title: action.payload,
+      };
+    case "SLUG_UPDATE":
+      return {
+        ...state,
+        slug: action.payload,
+      };
+    case "CONTENT_UPDATE":
+      return {
+        ...state,
+        content: action.payload,
+      };
+    case "DATE_UPDATE":
+      return {
+        ...state,
+        date: action.payload as Date | undefined,
+      };
+    case "RESET":
+      return initState;
+    default:
+      return state;
+  }
+}
+
 export function AddArchiveSheet() {
   const [open, setOpen] = useState(false);
-  const [title, setTitle] = useState("");
-  const [slug, setSlug] = useState("");
-  const [content, setContent] = useState("");
-  const [date, setDate] = useState<Date | undefined>();
+
+  const [state, dispatch] = useReducer(reducer, initState);
 
   // Auto-generate slug
   function updateTitleAndSlug(value: string) {
-    setTitle(value);
+    dispatch({ type: "TITLE_UPDATE", payload: value });
     let generatedSlug = "";
 
     if (value) {
@@ -51,13 +99,15 @@ export function AddArchiveSheet() {
         .replace(/[^a-z0-9\s-]/g, "")
         .replace(/\s+/g, "-");
     }
-    setSlug(() => generatedSlug);
+
+    dispatch({ type: "SLUG_UPDATE", payload: generatedSlug });
   }
 
   const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: async () => {
+      const { title, slug, content, date } = state;
       if (!date) return;
 
       const localISODate = new Date(
@@ -73,15 +123,13 @@ export function AddArchiveSheet() {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["articles"] });
+      dispatch({ type: "RESET" });
       setOpen(false);
-      setTitle("");
-      setSlug("");
-      setContent("");
-      setDate(undefined);
     },
   });
 
   async function handleSubmit() {
+    const { title, slug, content, date } = state;
     if (!title || !slug || !content || !date) return;
 
     mutation.mutate();
@@ -100,14 +148,14 @@ export function AddArchiveSheet() {
           <div className="space-y-2.5">
             <Label>Title</Label>
             <Input
-              value={title}
+              value={state.title}
               onChange={(e) => updateTitleAndSlug(e.target.value)}
             />
           </div>
 
           <div className="space-y-2.5">
             <Label>Slug (auto)</Label>
-            <Input value={slug} readOnly />
+            <Input value={state.slug} readOnly />
           </div>
 
           <div className="space-y-2.5">
@@ -116,15 +164,25 @@ export function AddArchiveSheet() {
               <PopoverTrigger asChild>
                 <Button
                   variant="outline"
-                  data-empty={!date}
+                  data-empty={!state.date}
                   className="data-[empty=true]:text-muted-foreground w-54 md:w-[280px] justify-start text-left font-normal"
                 >
                   <CalendarIcon className="mr-2 h-4 w-4" />
-                  {date ? format(date, "PPP") : <span>Pick a date</span>}
+                  {state.date ? (
+                    format(state.date, "PPP")
+                  ) : (
+                    <span>Pick a date</span>
+                  )}
                 </Button>
               </PopoverTrigger>
               <PopoverContent className="w-auto p-0">
-                <Calendar mode="single" selected={date} onSelect={setDate} />
+                <Calendar
+                  mode="single"
+                  selected={state.date}
+                  onSelect={(selectedDate) =>
+                    dispatch({ type: "DATE_UPDATE", payload: selectedDate })
+                  }
+                />
               </PopoverContent>
             </Popover>
           </div>
@@ -133,9 +191,11 @@ export function AddArchiveSheet() {
             <Label className="space-y-2.5">Content</Label>
             <MdEditor
               style={{ height: "300px" }}
-              value={content}
+              value={state.content}
               renderHTML={(text) => <ReactMarkdown>{text}</ReactMarkdown>}
-              onChange={({ text }) => setContent(text)}
+              onChange={({ text }) =>
+                dispatch({ type: "CONTENT_UPDATE", payload: text })
+              }
             />
           </div>
 
